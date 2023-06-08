6b621d9f720572964352ddeb99b8ca382a3e4101
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-08 20:45:59 +0530

    ok

---
+++ src/test/java/jdbc/StreamAPI_Ex.java
@@ -0,0 +1,11 @@
+package jdbc;
+
+import java.util.Arrays;
+
+public class StreamAPI_Ex
+{
+    public static void main(String[] args)
+    {
+        Arrays.asList(1,2,3,4,5,6,7,8,9).stream().map((n)->n*n).forEach(System.out::println);
+    }
+}
